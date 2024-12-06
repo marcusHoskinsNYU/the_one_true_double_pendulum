d46da4ab6e37de5c44ba5ca32fb958e69469f3e0
kdindial <knd286@nyu.edu>
2024-12-06 01:29:37 -0500

    fixed error in integrator. also added some results

--- integrators/rk4.py
+++ integrators/rk4.py
@@ -4,7 +4,7 @@ import math
 import matplotlib.pyplot as plt
 import os
 
-cwd='/Users/Krishna/Documents/repositories/the_one_true_double_pendulum'
+path='/Users/Krishna/Documents/repositories/the_one_true_double_pendulum'
 
 '''
 #if you are not krishna, then uncomment this code
@@ -33,13 +33,13 @@ def f(r):
     omega1=r[2]
     omega2=r[3]
 
-    num1 = np.float64((omega1**2)) * np.sin(2*theta1 - 2*theta2) + 2 * np.float64((omega2**2))*np.sin(theta1-theta2) + (g/l)*(np.sin(theta1 - 2*theta2) + 3*np.sin(theta1))
+    num1 = - ( (omega1**2) * np.sin(2*theta1 - 2*theta2) + 2 * (omega2**2)*np.sin(theta1-theta2) + (g/l)*(np.sin(theta1 - 2*theta2) + 3*np.sin(theta1)))
     den1 = 3 - np.cos(2*theta1 - 2*theta2)
 
     num2 = 4 * np.float64((omega1**2)) * np.sin(theta1 - theta2) + np.float64((omega2**2)) * np.sin(2*theta1-2*theta2) + (2*g/l)*(np.sin(2*theta1 - theta2) - np.sin(theta2))
     den2 = 3 - np.cos(2 * theta1 - 2 * theta2)
 
-    #require theta 1  and theta2 to be between 0 and 2pi
+    
     f_theta1=omega1 
     f_theta2=omega2  
     f_omega1=num1/den1
@@ -57,13 +57,12 @@ initial_vals=np.array([ theta1 , theta2 , omega1, omega2], np.float64) # a vecto
 
 
 
-def rk4(a: float, b: float, N: int, f, r: np.ndarray):
+def rk4(t: float, N: int, f, r: np.ndarray):
     """
     4th-order RK integrator with linear step size.
 
     Inputs:
-    a  = initial time value
-    b  = final time value
+    t  = final time value
     N  = number of time steps
     f  = function representing the differential equation
     r  = initial state [theta1, theta2, omega1, omega2]
@@ -71,9 +70,9 @@ def rk4(a: float, b: float, N: int, f, r: np.ndarray):
     Outputs:
     Returns an Nx5 array of [theta1, theta2, omega1, omega2, t].
     """
-    h = (b - a) / N
-    tpoints = np.linspace(a, b, N)
-    results = np.zeros((N, 5))
+    h = t/ N
+    tpoints = np.arange(0, t, h)
+    results = np.zeros((N, len(r)+1))
 
     for i, t in enumerate(tpoints):
         # Store current state and time
@@ -87,14 +86,11 @@ def rk4(a: float, b: float, N: int, f, r: np.ndarray):
 
         # Update state
         r = r + (k1 + 2 * k2 + 2 * k3 + k4) / 6
-        # Clamp values if necessary to prevent overflow
-        max_value = 1e7
-        if np.any(np.abs(r[3]) > max_value):
-            raise ValueError(f"Omega2 exceeded max_value={max_value} at step {i}, time={t:.3f}. Current state: {r}")
-
     return results
 
-N_steps=100000
+N_steps=100
+t_final=20
 
-partb=rk4(a=0.0,b=100, N=N_steps,  f=f,r=initial_vals.copy())
-np.savetxt( f'{cwd}/integratorResults/partb_rk4_{N_steps/1000}k_timesteps_100s.csv', partb, delimiter=',', header='theta1,theta2,omega1,omega2,t', comments='', fmt='%f')
+deltaT=t_final/N_steps
+partb=rk4(t=t_final, N=N_steps,  f=f,r=initial_vals.copy())
+np.savetxt( f'{path}/results/partb_rk4_{t_final}s_{deltaT*1000}ms_timesteps.csv', partb, delimiter=',', header='theta1,theta2,omega1,omega2,t', comments='', fmt='%f') 
